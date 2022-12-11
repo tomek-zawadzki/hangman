44c90a493f4d3d17fe23533216b146ca5218c8f5
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-12-11 21:13:19 +0100

    implementing show category

--- app/game.js
+++ app/game.js
@@ -1,16 +1,39 @@
 class Game {
+  quotes = [
+    {
+      text: "pan tadeusz",
+      category: "Utwór literacki",
+    },
+    {
+      text: "janko muzykant",
+      category: "Utwór literacki",
+    },
+    {
+      text: "akademia pana kleksa",
+      category: "Film",
+    },
+    {
+      text: "ogniem i mieczem",
+      category: "Film",
+    },
+  ];
+
   constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
     this.lettersWrapper = lettersWrapper;
     this.categoryWrapper = categoryWrapper;
     this.wordWrapper = wordWrapper;
     this.outputWrapper = outputWrapper;
+
+    const { text, category } =
+      this.quotes[Math.floor(Math.random() * this.quotes.length)];
+    this.categoryWrapper.innerHTML = category;
   }
 
   guess(letter) {
     console.log(letter);
   }
 
-  start() {
+  drawLetters() {
     for (let i = 0; i < 26; i++) {
       const label = (i + 10).toString(36);
       const button = document.createElement("button");
@@ -18,5 +41,9 @@ class Game {
       button.addEventListener("click", () => this.guess(label));
       this.lettersWrapper.appendChild(button);
     }
+  }
+
+  start() {
+    this.drawLetters();
   }
 }

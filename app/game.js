432bdbf2f861b0b445c478272aad9b0faf15261c
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-12-11 21:01:39 +0100

    created buttons with letters

---
+++ app/game.js
@@ -0,0 +1,22 @@
+class Game {
+  constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
+    this.lettersWrapper = lettersWrapper;
+    this.categoryWrapper = categoryWrapper;
+    this.wordWrapper = wordWrapper;
+    this.outputWrapper = outputWrapper;
+  }
+
+  guess(letter) {
+    console.log(letter);
+  }
+
+  start() {
+    for (let i = 0; i < 26; i++) {
+      const label = (i + 10).toString(36);
+      const button = document.createElement("button");
+      button.innerHTML = label;
+      button.addEventListener("click", () => this.guess(label));
+      this.lettersWrapper.appendChild(button);
+    }
+  }
+}
